4e3983d5edd655209a676e5eaf48ed026c8ebd00
unknown <naovalm>
2023-01-07 22:06:27 +0700

    adding ruang informasi & jadwal

---
+++ resources/views/jadwalkonselor.blade.php
@@ -0,0 +1,187 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Konseling kuy</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="style-2.css">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Konseling Kuy</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav ms-auto mb-1">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#home">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Jadwal Konselor</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Program</a>
+            </li>
+            <li class="nav-item dropdown" >
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Nama
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Profile</a></li>
+                <li><a class="dropdown-item" href="#">Logout</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="cardslide">
+              <div class="card jadwal">
+                <h3 class="card-title">Senin</h3>
+                <div class="card-body namajam">
+                  <div class="jadwalleft">
+                    <h5 class="card-text">Konselor</h5>
+                    <h5 class="card-text">Konselor</h5>
+                    <h5 class="card-text">Konselor</h5>
+                  </div>
+                  <div class="jadwalright">
+                    <h5>pukul : </h5>
+                    <h5>pukul : </h5>
+                    <h5>pukul : </h5> 
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="cardslide">
+              <div class="card jadwal">
+                <h3 class="card-title">Selasa</h3>
+                <div class="card-body namajam">
+                  <div class="jadwalleft">
+                    <h5 class="card-text">Konselor</h5>
+                    <h5 class="card-text">Konselor</h5>
+                    <h5 class="card-text">Konselor</h5>
+                  </div>
+                  <div class="jadwalright">
+                    <h5>pukul : </h5>
+                    <h5>pukul : </h5>
+                    <h5>pukul : </h5> 
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="carousel-item">
+            <div class="cardslide">
+              <div class="card jadwal">
+                <h3 class="card-title">Rabu</h3>
+                <div class="card-body namajam">
+                  <div class="jadwalleft">
+                    <h5 class="card-text">Konselor</h5>
+                    <h5 class="card-text">Konselor</h5>
+                    <h5 class="card-text">Konselor</h5>
+                  </div>
+                  <div class="jadwalright">
+                    <h5>pukul : </h5>
+                    <h5>pukul : </h5>
+                    <h5>pukul : </h5> 
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="cardslide">
+              <div class="card jadwal">
+                <h3 class="card-title">Kamis</h3>
+                <div class="card-body namajam">
+                  <div class="jadwalleft">
+                    <h5 class="card-text">Konselor</h5>
+                    <h5 class="card-text">Konselor</h5>
+                    <h5 class="card-text">Konselor</h5>
+                  </div>
+                  <div class="jadwalright">
+                    <h5>pukul : </h5>
+                    <h5>pukul : </h5>
+                    <h5>pukul : </h5> 
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="carousel-item">
+            <div class="cardslide">
+              <div class="card jadwal">
+                <h3 class="card-title">Jumat</h3>
+                <div class="card-body namajam">
+                  <div class="jadwalleft">
+                    <h5 class="card-text">Konselor</h5>
+                    <h5 class="card-text">Konselor</h5>
+                    <h5 class="card-text">Konselor</h5>
+                  </div>
+                  <div class="jadwalright">
+                    <h5>pukul : </h5>
+                    <h5>pukul : </h5>
+                    <h5>pukul : </h5> 
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="cardslide">
+              <div class="card jadwal">
+                <h3 class="card-title">Sabtu</h3>
+                <div class="card-body namajam">
+                  <div class="jadwalleft">
+                    <h5 class="card-text">Konselor</h5>
+                    <h5 class="card-text">Konselor</h5>
+                    <h5 class="card-text">Konselor</h5>
+                  </div>
+                  <div class="jadwalright">
+                    <h5>pukul : </h5>
+                    <h5>pukul : </h5>
+                    <h5>pukul : </h5> 
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="carousel-item">
+            <div class="cardslide">
+              <div class="card jadwal">
+                <h3 class="card-title">Minggu</h3>
+                <div class="card-body namajam">
+                  <div class="jadwalleft">
+                    <h5 class="card-text">Konselor</h5>
+                    <h5 class="card-text">Konselor</h5>
+                    <h5 class="card-text">Konselor</h5>
+                  </div>
+                  <div class="jadwalright">
+                    <h5>pukul : </h5>
+                    <h5>pukul : </h5>
+                    <h5>pukul : </h5> 
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
